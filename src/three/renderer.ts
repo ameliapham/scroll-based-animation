bb638e3ef08c965caf07d250e944722d02bda098
ameliapham <ptt.huong.8895@gmail.com>
2026-01-09 01:09:00 +0100

    Transparent background

--- src/three/renderer.ts
+++ src/three/renderer.ts
@@ -7,7 +7,10 @@ type RendererParams = {
 export function createRenderer(props: RendererParams): THREE.WebGLRenderer {
     const { canvas } = props;
     
-    const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+    const renderer = new THREE.WebGLRenderer({ 
+        canvas: canvas,
+        alpha : true, // transparent background
+    });
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     return renderer;
